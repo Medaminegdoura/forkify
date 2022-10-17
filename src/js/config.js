fc3fe3d8cc9037d7c79207e3a59f116deaaaae50
Medaminegdoura <medaminegdoura@gmail.com>
2022-10-17 23:07:18 +0100

    ToDeploy v1.3

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = 'da96a717-5800-47a1-8bcb-f1b9dff20c1d';
+export const KEY = '7cfd9485-92e3-44c8-9385-a46c3b9184fd';
 export const MODAL_CLOSE_SEC = 1.5;
